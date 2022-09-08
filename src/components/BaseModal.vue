3e1af202df1e948c10a03db9023cd8f141561cde
Jorge <jorgeroca1996@gmail.com>
2022-09-08 16:50:24 +0200

    Prettier y ESLint config

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,12 +1,20 @@
 <template>
   <Teleport to="body">
     <Transition name="modal-outer">
-      <div v-show="modalActive"
-        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+      <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      >
         <Transition name="modal-inner">
-          <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md"
+          >
             <slot />
-            <button class="text-white mt-8 bg-weather-primary py-2 px-6" @click="$emit('close-modal')">
+            <button
+              class="text-white mt-8 bg-weather-primary py-2 px-6"
+              @click="$emit('close-modal')"
+            >
               Close
             </button>
           </div>
@@ -17,7 +25,7 @@
 </template>
 
 <script setup>
-defineEmits(["close-modal"])
+defineEmits(["close-modal"]);
 defineProps({
   modalActive: {
     type: Boolean,
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="sticky top-0 bg-weather-primary shadow-lg">
-    <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
       <RouterLink :to="{ name: 'home' }">
         <div class="flex items-center gap-3">
           <i class="fa-solid fa-sun text-2xl"></i>
@@ -9,9 +11,13 @@
       </RouterLink>
 
       <div class="flex gap-3 flex-1 justify-end">
-        <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
-          @click="toggleModal"></i>
-        <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+        <i
+          class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+          @click="toggleModal"
+        ></i>
+        <i
+          class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+        ></i>
       </div>
 
       <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
